d9ab967227933561a96b71b8029eff397816a5db
yihu-lv <1720427782@qq.com>
2021-05-25 12:42:49 +0800

    switch的开关，动画，

---
+++ src/lib/Switch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <button :class="{checked:value}" @click="toggle"><span></span></button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:{
+    value:Boolean
+  },
+  setup(props,context) {
+    const toggle = ()=>{
+      context.emit('input',!props.value)
+    }
+    return { toggle }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$h:22px;
+$h2:$h - 4px;
+button {
+  height: $h;
+  width: $h*2;
+  background: gray;
+  border-radius: $h/2;
+  border: none;
+  position: relative;
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    border-radius: $h2/2;
+    background: white;
+    transition: left 0.6s;
+  }
+}
+button.checked {
+  background: blue;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+
+</style>
