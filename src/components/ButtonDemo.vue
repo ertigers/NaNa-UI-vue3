d295dc58ddb1846fc893579e726df84fd3b07a76
yihu-lv <1720427782@qq.com>
2021-05-25 14:30:54 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,69 +1,20 @@
 <template>
   <div>
-    <p>button</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
-    <p>switch</p>
+    <Button @click="log" size="small">
+      点击这里
+    </Button>
   </div>
 </template>
+
+<script>
+import Button from '../lib/Button.vue'
+export default {
+  components:{ Button },
+  setup() {
+    const log = ()=>{
+      console.log("点击了，打印一下")
+    }
+    return { log }
+  }
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper" :size="size">
+    <button v-bind="$attrs">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs:false,
+  setup(props,context) {
+    const {size, ...rest} = context.attrs
+    return {size,rest}
+  }
+}
+</script>
+
+<style lang="scss">
+.wrapper {
+  border: 1px solid red;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
