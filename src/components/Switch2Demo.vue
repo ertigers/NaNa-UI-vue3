0ae45b8a1b27b2168219b04c3e84fbd921731862
yihu-lv <1720427782@qq.com>
2021-05-28 21:29:15 +0800

    展示了代码

--- src/components/Switch2Demo.vue
+++ src/components/Switch2Demo.vue
@@ -1,3 +1,7 @@
+<demo>
+支持disable
+</demo>
+
 <template>
 <Switch v-model:value="bool" disabled />
 </template>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      <pre>{{Switch1Demo.__sourceCode}}</pre>
     </div>
   </div>
   <div class="demo">
@@ -22,7 +22,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      <pre>{{ Switch2Demo.__sourceCode }}</pre>
     </div>
   </div>
 </div>
@@ -33,6 +33,7 @@ import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1Demo.vue'
 import Switch2Demo from './Switch2Demo.vue'
+
 import {
   ref
 } from 'vue'
@@ -46,7 +47,7 @@ export default {
   setup() {
     const bool = ref(false)
     return {
-      bool
+      bool,Switch1Demo,Switch2Demo
     }
   }
 }
