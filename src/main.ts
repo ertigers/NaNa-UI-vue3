e640d822cbcfd27c9121968e413b811547c769a8
yihu-lv <1720427782@qq.com>
2021-05-28 16:34:37 +0800

    添加markdown输入方式，优化代码

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import 'github-markdown-css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router';
-
+import Markdown from './components/Markdown.vue'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.component("markdown",Markdown)
