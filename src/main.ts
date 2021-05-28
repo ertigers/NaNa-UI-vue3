0ccf5a62f5517ac24d624bb60e6f983be8d7ef51
yihu-lv <1720427782@qq.com>
2021-05-28 20:35:54 +0800

    spell wrong

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,4 @@ import Markdown from './components/Markdown.vue'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-app.component("markdown",Markdown)
+app.component("Markdown",Markdown)
