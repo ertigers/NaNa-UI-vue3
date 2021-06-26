704ccda55c8e4778a60514766187bd22b790986e
yihu-lv <1720427782@qq.com>
2021-06-26 22:01:24 +0800

    新增Input组件

---
+++ src/lib/Input.vue
@@ -0,0 +1,91 @@
+<template>
+  <input class="nana-input" 
+    :class="classes" 
+    :type="type" 
+    :clearable="clearable"
+    :disabled="disabled" 
+    :placeholder="placeholder">
+</template>
+
+<script>
+import { computed } from 'vue';
+export default {
+  props: {
+    size: {
+      type: String,
+      default: "normal",
+    },
+    radius: {
+      type: String,
+      default: "normal",
+    },
+    type: {
+      type: String,
+      default: "normal",
+    },
+    clearable: {
+      type: String,
+      default: "",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: "请输入~",
+    }
+  },
+  setup(props) {
+    const { size, radius } = props;
+    const classes = computed(() => {
+      return {
+        [`nana-input-size-${size}`]: size,
+        [`nana-input-radius-${radius}`]: radius,
+      };
+    });
+    return { classes };
+  },
+}
+</script>
+
+<style lang="scss">
+$grey: rgb(196, 196, 196);
+.nana-input {
+  border: 1px solid  $grey;
+  padding: 10px 6px;
+  border-radius: 4px;
+  & + & {
+    margin-left: 14px;
+  }
+  &:hover,
+  &:focus {
+    color: rgb(0, 0, 0);
+    border-color: rgb(50, 171, 211);
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.nana-input-size-small {
+    padding: 8px 4px;
+  }
+  &.nana-input-size-big {
+    padding: 14px 8px;
+
+  }
+}
+
+</style>
+
+
+
+
+
+
+
+
+
+
--- src/router.ts
+++ src/router.ts
@@ -1,35 +1,40 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
 
-import SwitchDemo from './components/switch/SwitchDemo.vue'
-import ButtonDemo from './components/button/ButtonDemo.vue'
-import DialogDemo from './components/dialog/DialogDemo.vue'
-import TabsDemo from './components/tabs/TabsDemo.vue'
+import Markdown from "./components/Markdown.vue";
+import intro from "./markdown/intro.md";
+import getStarted from "./markdown/get-started.md";
+import install from "./markdown/install.md";
 
-import Markdown from './components/Markdown.vue'
-import intro from './markdown/intro.md'
-import getStarted from './markdown/get-started.md'
-import install from './markdown/install.md'
+import SwitchDemo from "./components/switch/SwitchDemo.vue";
+import ButtonDemo from "./components/button/ButtonDemo.vue";
+import DialogDemo from "./components/dialog/DialogDemo.vue";
+import TabsDemo from "./components/tabs/TabsDemo.vue";
+import InputDemo from "./components/input/InputDemo.vue";
 
-import { h } from 'vue';
+import { h } from "vue";
 
-const history = createWebHashHistory()
-const md = string => h(Markdown, { content: string, key: string })
+const history = createWebHashHistory();
+const md = (string) => h(Markdown, { content: string, key: string });
 export const router = createRouter({
   history: history,
   routes: [
-    {path: '/',component: Home},
-    {path: '/doc',component: Doc,
-      children:[
-        {path: '',redirect:'/doc/intro'},
+    { path: "/", component: Home },
+    {
+      path: "/doc",
+      component: Doc,
+      children: [
+        { path: "", redirect: "/doc/intro" },
         { path: "intro", component: md(intro) },
         { path: "get-started", component: md(getStarted) },
         { path: "install", component: md(install) },
-        {path: 'switch',component: SwitchDemo},
-        {path: 'button',component: ButtonDemo},
-        {path: 'dialog',component: DialogDemo},
-        {path: 'tabs',component: TabsDemo},
-    ]}
-  ]
-})
+        { path: "switch", component: SwitchDemo },
+        { path: "button", component: ButtonDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "tabs", component: TabsDemo },
+        { path: "input", component: InputDemo },
+      ],
+    },
+  ],
+});
